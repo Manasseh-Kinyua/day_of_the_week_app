7acc09623d3dca6cb12526bd0aa80453514558e2
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2021-12-13 09:54:54 +0300

    Add conditional statements to evaluate the Male Akan Names

--- javascript/main.js
+++ javascript/main.js
@@ -16,4 +16,20 @@ let dayOfTHeWeek =  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%
 
 let maleAkanName = ['Kwasi', 'Kwadwo', 'Kwabena','Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 let femaleAkanName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+if(gender==='female' && dayOfTheWeek===1) {
+    alert('Your Akan name is ' + akanFemaleName['0'])
+  }else if(gender==='female' && dayOfTheWeek==2) {
+    alert('Your Akan name is ' + akanFemaleName['1'])
+  }else if(gender==='female' && dayOfTheWeek==3) {
+    alert('Your Akan name is ' + akanFemaleName['2'])
+  }else if(gender==='female' && dayOfTheWeek==4) {
+    alert('Your Akan name is ' + akanFemaleName['3'])
+  }else if(gender==='female' && dayOfTheWeek==5) {
+    alert('Your Akan name is ' + akanFemaleName['4'])
+  }else if(gender==='female' && dayOfTheWeek==6) {
+    alert('Your Akan name is ' + akanFemaleName['5'])
+  }else if(gender==='female' && dayOfTheWeek==7) {
+    alert('Your Akan name is ' + akanFemaleName['6'])
+  }
 }
