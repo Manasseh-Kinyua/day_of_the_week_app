78d8fc122f571a1ff3b0e0f790c602c6e9ab620a
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2021-12-13 09:56:59 +0300

    Add conditional statements to evaluate the female Akan Names

--- javascript/main.js
+++ javascript/main.js
@@ -17,6 +17,22 @@ let dayOfTHeWeek =  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%
 let maleAkanName = ['Kwasi', 'Kwadwo', 'Kwabena','Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 let femaleAkanName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
+if(gender==='male' && dayOfTheWeek===1) {
+    alert('Your Akan name is ' + akanMaleName['0'])
+  }else if(gender==='male' && dayOfTheWeek===2) {
+    alert('Your Akan name is ' + akanMaleName['1'])
+  }if(gender==='male' && dayOfTheWeek===3) {
+    alert('Your Akan name is ' + akanMaleName['2'])
+  }if(gender==='male' && dayOfTheWeek===4) {
+    alert('Your Akan name is ' + akanMaleName['3'])
+  }if(gender==='male' && dayOfTheWeek===5) {
+    alert('Your Akan name is ' + akanMaleName['4'])
+  }if(gender==='male' && dayOfTheWeek===6) {
+    alert('Your Akan name is ' + akanMaleName['5'])
+  }if(gender==='male' && dayOfTheWeek===7) {
+    alert('Your Akan name is ' + akanMaleName['6'])
+  }
+
 if(gender==='female' && dayOfTheWeek===1) {
     alert('Your Akan name is ' + akanFemaleName['0'])
   }else if(gender==='female' && dayOfTheWeek==2) {
