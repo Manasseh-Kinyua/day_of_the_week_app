e61d5c41520f49db54af3514e3d8c8d251efe009
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2021-12-13 09:15:29 +0300

    Make variables for year, month and date of birth in js

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+function myButton(event) {
+    event.preventDefault()
+
+var year = document.getElementById('year').value;
+var month = parseInt(document.getElementById('month').value);
+var date = parseInt(document.getElementById('date').value);
+var gender = document.querySelector('input[name = gender] : checked').value;
+}
