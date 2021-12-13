8d522fd4daab7e548cab2417aa643214ad1e0c7e
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2021-12-13 09:47:48 +0300

    Add two separate arrays to store the male and female Akan names

--- javascript/main.js
+++ javascript/main.js
@@ -7,10 +7,13 @@ var month = parseInt(document.getElementById('month').value);
 var date = parseInt(document.getElementById('date').value);
 var gender = document.querySelector('input[name = gender] : checked').value;
 
-const cc = year.substring(0, 2);
-const yy = year.substring(2, 3);
-const mm = month;
-const dd = date;
+let cc = year.substring(0, 2);
+let yy = year.substring(2, 3);
+let mm = month;
+let dd = date;
 
 let dayOfTHeWeek =  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )% 7;
+
+let maleAkanName = ['Kwasi', 'Kwadwo', 'Kwabena','Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+let femaleAkanName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 }
