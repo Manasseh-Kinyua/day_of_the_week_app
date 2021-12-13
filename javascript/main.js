e7f8528febaa38a7e4bccba0321fa59e778fb947
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2021-12-13 10:20:21 +0300

    Add some modification to my javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 function myButton(event) {
     event.preventDefault()
-
+}
 
 var year = document.getElementById('year').value;
 var month = parseInt(document.getElementById('month').value);
@@ -18,34 +18,34 @@ let maleAkanName = ['Kwasi', 'Kwadwo', 'Kwabena','Kwaku', 'Yaw', 'Kofi', 'Kwame'
 let femaleAkanName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
 if(gender==='male' && dayOfTheWeek===1) {
-    alert('Your Akan name is ' + akanMaleName['0'])
+    alert('Your Akan name is ' + maleAkanName['0'])
   }else if(gender==='male' && dayOfTheWeek===2) {
-    alert('Your Akan name is ' + akanMaleName['1'])
+    alert('Your Akan name is ' + maleAkanName['1'])
   }if(gender==='male' && dayOfTheWeek===3) {
-    alert('Your Akan name is ' + akanMaleName['2'])
+    alert('Your Akan name is ' + maleAkanName['2'])
   }if(gender==='male' && dayOfTheWeek===4) {
-    alert('Your Akan name is ' + akanMaleName['3'])
+    alert('Your Akan name is ' + maleAkanName['3'])
   }if(gender==='male' && dayOfTheWeek===5) {
-    alert('Your Akan name is ' + akanMaleName['4'])
+    alert('Your Akan name is ' + maleAkanName['4'])
   }if(gender==='male' && dayOfTheWeek===6) {
-    alert('Your Akan name is ' + akanMaleName['5'])
+    alert('Your Akan name is ' + maleAkanName['5'])
   }if(gender==='male' && dayOfTheWeek===7) {
-    alert('Your Akan name is ' + akanMaleName['6'])
+    alert('Your Akan name is ' + maleAkanName['6'])
   }
 
 if(gender==='female' && dayOfTheWeek===1) {
-    alert('Your Akan name is ' + akanFemaleName['0'])
+    alert('Your Akan name is ' + femaleAkanName['0'])
   }else if(gender==='female' && dayOfTheWeek==2) {
-    alert('Your Akan name is ' + akanFemaleName['1'])
+    alert('Your Akan name is ' + femaleAkanName['1'])
   }else if(gender==='female' && dayOfTheWeek==3) {
-    alert('Your Akan name is ' + akanFemaleName['2'])
+    alert('Your Akan name is ' + femaleAkanName['2'])
   }else if(gender==='female' && dayOfTheWeek==4) {
-    alert('Your Akan name is ' + akanFemaleName['3'])
+    alert('Your Akan name is ' + femaleAkanName['3'])
   }else if(gender==='female' && dayOfTheWeek==5) {
-    alert('Your Akan name is ' + akanFemaleName['4'])
+    alert('Your Akan name is ' + femaleAkanName['4'])
   }else if(gender==='female' && dayOfTheWeek==6) {
-    alert('Your Akan name is ' + akanFemaleName['5'])
+    alert('Your Akan name is ' + femaleAkanName['5'])
   }else if(gender==='female' && dayOfTheWeek==7) {
-    alert('Your Akan name is ' + akanFemaleName['6'])
+    alert('Your Akan name is ' + femaleAkanName['6'])
   }
 }
