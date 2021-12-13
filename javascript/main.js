5f2d1ebc362d255b16af9f10509e8094dbfc7445
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2021-12-13 09:37:47 +0300

    Make constant variables for the century, year, month and day

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,14 @@
 function myButton(event) {
     event.preventDefault()
 
+
 var year = document.getElementById('year').value;
 var month = parseInt(document.getElementById('month').value);
 var date = parseInt(document.getElementById('date').value);
 var gender = document.querySelector('input[name = gender] : checked').value;
+
+const cc = year.substring(0, 2);
+const yy = year.substring(2, 3);
+const mm = month;
+const dd = date;
 }
