c51fee5f0ac73e90ae42b9a35ddb98cd1f8523da
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2021-12-13 09:40:49 +0300

    Add the formula to calculate day of the week one was born

--- javascript/main.js
+++ javascript/main.js
@@ -11,4 +11,6 @@ const cc = year.substring(0, 2);
 const yy = year.substring(2, 3);
 const mm = month;
 const dd = date;
+
+let dayOfTHeWeek =  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )% 7;
 }
